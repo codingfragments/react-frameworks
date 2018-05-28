3caf6df1149851d82fe3f79902c30d318d367104
stefan <smarx@marxworld.net>
2018-05-29 00:57:07 +0200

    Added coreui css dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,17 @@ import '@coreui/coreui/dist/css/coreui.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+
+
+// Styles
+import '@coreui/icons/css/coreui-icons.min.css';
+import 'flag-icon-css/css/flag-icon.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'simple-line-icons/css/simple-line-icons.css';
+import './gen-src/styles/style.css';
+
+
 
 ReactDOM.render(
   <App />,
