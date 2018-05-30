0ebbf68bc6a3cda01b100bbaac751bf1a1edd341
Stefan Marx <smarx@marxworld.net>
2018-05-30 14:49:47 +0200

    FirstRunning

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,83 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
+/* tslint:disable:no-console */
 
-class App extends React.Component {
+
+// import * as FontAwesome from "react-fontawesome";
+import * as FontAwesome from "react-fontawesome";
+import {Collapse,DropdownItem,DropdownMenu,DropdownToggle,Nav,Navbar,NavbarBrand,NavbarToggler,NavItem,NavLink,UncontrolledDropdown} from "reactstrap";
+
+
+interface IAppProps {
+    name:string
+}
+
+interface IAppState {
+    isOpen: boolean;
+}
+
+
+class App extends React.Component<IAppProps,IAppState> {
+    constructor(props: any) {
+        super(props);
+
+       this.state = {
+           isOpen:true
+       }
+    }
+
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title text-warning">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+        <div>
+            <Navbar color="light" light={true} expand="md" >
+                <NavbarBrand href="/">reactstrap</NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar={true} >
+                    <Nav  navbar={true}>
+                        <NavItem>
+                            <NavLink href="/components/">Components</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown nav={true} inNavbar={true}>
+                            <DropdownToggle nav={true} caret={true}>
+                                Options
+                            </DropdownToggle>
+                            <DropdownMenu right={true}>
+                                <DropdownItem>
+                                    Option 1
+                                </DropdownItem>
+                                <DropdownItem>
+                                    Option 2
+                                </DropdownItem>
+                                <DropdownItem divider={true} />
+                                <DropdownItem>
+                                    Reset
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </Nav>
+                </Collapse>
+                <FontAwesome name="users"   />
+
+            </Navbar>
+            
+        </div>
+
+
     );
   }
+
+  private toggle = () => {
+          this.setState({
+              isOpen: !this.state.isOpen});
+
+  }
+
+
+
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import '@coreui/coreui/dist/css/coreui.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
@@ -6,16 +5,24 @@ import registerServiceWorker from './registerServiceWorker';
 
 
 // Styles
-import '@coreui/icons/css/coreui-icons.min.css';
 import 'flag-icon-css/css/flag-icon.min.css';
-import 'font-awesome/css/font-awesome.min.css';
+import 'font-awesome/css/font-awesome.css';
 import 'simple-line-icons/css/simple-line-icons.css';
-import './gen-src/styles/style.css';
+import './styles/style.css';
+
+
+
+
+
+
+
+
 
 
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+  <App name="ksj"/>
+  ,
+  document.body as HTMLElement
 );
 registerServiceWorker();
