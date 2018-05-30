3249d99a1a967fcce66a517e7133b03eff42082a
Stefan Marx <smarx@marxworld.net>
2018-05-31 00:50:42 +0200

    Added typing for coreui-react

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,12 @@ import * as React from 'react';
 import './App.css';
 
 
+import { AppHeaderDropdown} from '@coreui/react';
+
 class App extends React.Component {
   public render() {
     return (
-      <div className="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
-          <header className="app-header navbar flex-row">
-              <span className="navbar-brand"> TESTER </span>
-                  <div className="nav navbar-nav flex-row">
-                  <div className="nav-item px-3">
-                      <a className="nav-link" href="#">Dashboard</a>
-                  </div>
-                  <div className="nav-item px-3">
-                      <a className="nav-link" href="#">Users</a>
-                  </div>
-                  <div className="nav-item px-3">
-                      <a className="nav-link" href="#">Settings</a>
-                  </div>
-              </div>
-              <div className="nav-item my-2 my-lg-0">...</div>
-          </header>
-      </div>
+        <AppHeaderDropdown>lsl</AppHeaderDropdown>
     );
   }
 }
---
+++ src/typings/coreui_react.d.ts
@@ -0,0 +1,205 @@
+/* tslint:disable:ban-types */
+/* tslint:disable:max-classes-per-file */
+/* tslint:disable:no-empty-interface */
+
+declare module '@coreui/react' {
+    import {ReactNode} from "react";
+
+    interface ICoreuiBaseProps {
+        tag ?: Function | string;
+        children?: ReactNode;
+        className?: string;
+        display?: string;
+        type?:string;
+    }
+
+    interface IAppAsideProps extends ICoreuiBaseProps {
+        fixed?: boolean;
+        hidden?: boolean;
+        isOpen?: boolean;
+        offCanvas?: boolean;
+    }
+
+    class AppAside extends React.Component<IAppAsideProps, {}> {
+    }
+
+    interface IAsideTogglerProps extends ICoreuiBaseProps {
+        mobile?: boolean;
+    }
+
+    class AsideToggler extends React.Component<IAsideTogglerProps, {}> {
+    }
+
+    /* 
+     *  ---------------------
+     *       AppBreadcrumb
+     *  ----------------------
+     */
+    interface IAppBreadcrumbProps extends ICoreuiBaseProps {
+        appRoutes ?: any
+    }
+
+    class AppBreadcrumb extends React.Component<IAppBreadcrumbProps, {}> {
+    }
+
+    /*
+     *  ---------------------
+     *       AppFooter
+     *  ----------------------
+     */
+    interface IAppFooterProps extends ICoreuiBaseProps {
+        fixed?:boolean;
+     }
+
+    class AppFooter extends React.Component<IAppFooterProps, {}> {
+    }
+
+    /*
+     *  ---------------------
+     *       AppHeader
+     *  ----------------------
+     */
+    interface IAppHeaderProps extends ICoreuiBaseProps {
+        fixed ?: boolean;
+     }
+
+    class AppHeader extends React.Component<IAppHeaderProps, {}> {
+    }
+
+    /*
+     *  ---------------------
+     *       AppHeaderDropdown
+     *  ----------------------
+     */
+
+    interface IAppHeaderDropdownProps {
+        children?: ReactNode;
+        direction?: string;
+    }
+    class AppHeaderDropdown extends React.Component<IAppHeaderDropdownProps,{}>{}
+
+    /*
+     *  ---------------------
+     *       AppNavbarBrand
+     *  ----------------------
+     */
+    interface IAppNavbarBrandProps extends ICoreuiBaseProps {
+        brand ?: any;
+        minimized ?: boolean;
+        full ?: boolean;
+    }
+
+    class AppNavbarBrand extends React.Component<IAppNavbarBrandProps, {}> {
+    }
+
+    /*
+     *  ---------------------
+     *       AppSidebar
+     *  ----------------------
+     */
+    interface IAppSidebarProps extends ICoreuiBaseProps {
+        compact ?: boolean;
+        fixed ?: boolean;
+        minimized ?: boolean;
+        isOpen ?: boolean;
+        offCanvas ?: boolean;
+        staticContext ?: any;
+    }
+
+    class AppSidebar extends React.Component<IAppSidebarProps, {}> {
+    }
+
+    /*
+     *  ---------------------
+     *       AppSidebarFooter
+     *  ----------------------
+     */
+    interface IAppSidebarFooterProps extends ICoreuiBaseProps {
+
+
+    }
+
+    class AppSidebarFooter extends React.Component<IAppSidebarFooterProps, {}> {
+    }
+
+    /*
+     *  ---------------------
+     *       AppSidebarForm
+     *  ----------------------
+     */
+    interface IAppSidebarFormProps extends ICoreuiBaseProps {
+
+
+    }
+
+    class AppSidebarForm extends React.Component<IAppSidebarFormProps, {}> {
+    }
+
+    /*
+     *  ---------------------
+     *     AppSidebarMinimizer
+     *  ----------------------
+     */
+    interface IAppSidebarMinimizerProps extends ICoreuiBaseProps {
+
+
+    }
+
+    class AppSidebarMinimizer extends React.Component<IAppSidebarMinimizerProps, {}> {
+    }
+
+    /*
+     *  ---------------------
+     *       AppSidebarNav
+     *  ----------------------
+     */
+    interface IAppSidebarNavProps extends ICoreuiBaseProps {
+        navConfig ?: any;
+        navFunc ?: Function | string;
+        isOpen ?: boolean;
+        staticContext?: any;
+    }
+
+    class AppSidebarNav extends React.Component<IAppSidebarNavProps, {}> {
+    }
+
+    /*
+     *  ---------------------
+     *       AppSidebarToggler
+     *  ----------------------
+     */
+    interface IAppSidebarTogglerProps extends ICoreuiBaseProps {
+        mobile?: boolean;
+    }
+
+    class AppSidebarToggler extends React.Component<IAppSidebarTogglerProps, {}> {
+    }
+
+    /*
+     *  ---------------------
+     *       AppSwitch
+     *  ----------------------
+     */
+    interface IAppSwitchProps extends ICoreuiBaseProps {
+        color ?: string;
+        label?: boolean;
+        outline ?: boolean | string;
+        size ?: string;
+        checked ?: boolean;
+        defaultChecked ?: boolean;
+        defaultValue ?: any;
+        value ?: string;
+        disabled ?: boolean;
+        form ?: any;
+        name ?: string;
+        required ?: boolean;
+        onChange ?: Function;
+        type ?: string;
+        variant ?: string;
+        dataOn ?: string;
+        dataOff ?: string;
+    }
+
+    class AppSwitch extends React.Component<IAppSwitchProps, {}> {
+    }
+}
