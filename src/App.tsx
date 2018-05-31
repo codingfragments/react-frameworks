cd937c0a3c505b833987bd3f5b6a5a24f76cfbce
Stefan Marx <smarx@marxworld.net>
2018-05-31 22:18:11 +0200

    SimpleStart

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import './App.css';
 
+import Button from '@material-ui/core/Button';
+
 import logo from './logo.svg';
 
 class App extends React.Component {
   public render() {
     return (
+      <>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -15,6 +18,10 @@ class App extends React.Component {
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
       </div>
+        <Button variant="raised" color="primary">
+          Hello World
+      </Button>
+      </>
     );
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import '@coreui/coreui/dist/css/coreui.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
@@ -6,11 +5,10 @@ import registerServiceWorker from './registerServiceWorker';
 
 
 // Styles
-import '@coreui/icons/css/coreui-icons.min.css';
 import 'flag-icon-css/css/flag-icon.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import 'simple-line-icons/css/simple-line-icons.css';
-import './gen-src/styles/style.css';
+// import './src/styles/style.css';
 
 
 
