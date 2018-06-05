546a7e749d273a1db9d0d5f4b63bcf08380c1216
Stefan Marx <smarx@marxworld.net>
2018-06-05 16:07:02 +0200

    AntDesign UI Boileplate, following https://ant.design/docs/react/use-in-typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
+import Button from "antd/lib/button/button";
 import * as React from 'react';
-import './App.css';
 
-import logo from './logo.svg';
+
+// import logo from './logo.svg';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title text-warning">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+          <Button type="primary"> Button press me </Button>
       </div>
     );
   }
